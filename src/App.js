e24843e188bfc6ac666d0e3e0668c120420d7a40
uchihaNayeem <uchiha.nayeem@gmail.com>
2020-11-19 19:33:56 +0600

    price list and navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  
+} from "react-router-dom";
+import PriceTable from './components/PriceTable/PriceTable';
+import Service from './components/Service/Service';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/price">
+          <Navbar></Navbar>
+          <PriceTable></PriceTable>
+        </Route>
+        <Route path="/service">
+          <Service></Service>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Service/Service.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Service = () => {
+  return (
+    <div>
+      <div class="container mb-5 mt-5">
+        <div class="row">
+          
+
+
+
+      
+      
+  
+
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
