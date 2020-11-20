c3bac5e18ca0e8a6c65cb0bfcad6bdf1006e5163
uchihaNayeem <uchiha.nayeem@gmail.com>
2020-11-20 20:49:35 +0600

    router removed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  
-} from "react-router-dom";
+
 import PriceTable from './components/PriceTable/PriceTable';
 import Service from './components/Service/Service';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/">
-          <Navbar></Navbar>
-          <Service></Service>
-          <PriceTable></PriceTable>
-        </Route>
-        <Route path="/service">
-          <Service></Service>
-        </Route>
-      </Switch>
-    </Router>
+    <div className="imageBg">
+      <Navbar></Navbar>
+      <Service></Service>
+      <PriceTable></PriceTable>
+      
+      
+    </div>
   );
 }
 
