723f6405b5aa4baa3352980d3654ca409bff1c99
uchihaNayeem <uchiha.nayeem@gmail.com>
2020-11-20 12:43:39 +0600

    hamburger works now in mobile

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
             <nav className="navbar navbar-expand-lg navbar-expand-md pt-2 ">
                     <img src={logo} alt="" width="100px"/>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon" style={{color: 'black'}}>
+                    <span className="navbar-toggler-icon" style={{color: 'black', textDecoration: 'none'}}>
                     <FontAwesomeIcon icon={faBars} />
                     </span>
                 </button>
