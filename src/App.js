5b5bfb716167536253f23a2ed4156d947b3cfcd4
Tanvir Ibn Touhid <tanvirbintouhid5230@gmail.com>
2020-11-20 07:16:21 +0600

    achievement section added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import PriceTable from "./components/PriceTable/PriceTable";
 import Service from "./components/Service/Service";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import Achievement from "./components/achievement/Achievement";
 
 function App() {
   return (
     <div className="imageBg">
       <Navbar></Navbar>
+      <Achievement />
       <PriceTable></PriceTable>
       <Service></Service>
       <Footer />
