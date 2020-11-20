388745e9ad564cf192029ec15885ca3cdb629484
Tanvir Ibn Touhid <tanvirbintouhid5230@gmail.com>
2020-11-20 06:19:03 +0600

    footer done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  
-} from "react-router-dom";
-import PriceTable from './components/PriceTable/PriceTable';
-import Service from './components/Service/Service';
-import Navbar from './components/Navbar/Navbar';
+import "./App.css";
+import PriceTable from "./components/PriceTable/PriceTable";
+import Service from "./components/Service/Service";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/price">
-          <Navbar></Navbar>
-          <PriceTable></PriceTable>
-        </Route>
-        <Route path="/service">
-          <Service></Service>
-        </Route>
-      </Switch>
-    </Router>
+    <div className="imageBg">
+      <Navbar></Navbar>
+      <PriceTable></PriceTable>
+      <Service></Service>
+      <Footer />
+    </div>
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,42 +1,60 @@
-import React from 'react';
-import './Navbar.css'
-import logo from '../../Illustration/Group 86.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import "./Navbar.css";
+import logo from "../../Illustration/Group 86.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
     <div className="container-md">
-            <nav className="navbar navbar-expand-lg navbar-expand-md pt-2 ">
-                    <img src={logo} alt="" width="100px"/>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon" style={{color: 'pink'}}>
-                    <FontAwesomeIcon icon={faBars} />
-                    </span>
-                </button>
+      <nav className="navbar navbar-expand-lg navbar-expand-md pt-2 ">
+        <img src={logo} alt="" width="100px" />
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" style={{ color: "pink" }}>
+            <FontAwesomeIcon icon={faBars} />
+          </span>
+        </button>
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ml-auto mt-2">
-                        <li className="nav-item ">
-                            <a className="nav-link ml-3" href="#">Home <span className="sr-only">(current)</span></a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link ml-3" href="#">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link ml-3" href="#">Work</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link ml-3" href="#">Blog</a>
-                        </li>
-                        <li className="nav-item mr-3 ml-3">
-                            <a className="nav-link" href="#">Contact</a>
-                        </li>
-                        <button className="btn customButton">Contact us</button>
-                    </ul>
-                </div>
-            </nav>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto mt-2">
+            <li className="nav-item ">
+              <a className="nav-link ml-3" href="#">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link ml-3" href="#">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link ml-3" href="#">
+                Work
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link ml-3" href="#">
+                Blog
+              </a>
+            </li>
+            <li className="nav-item mr-3 ml-3">
+              <a className="nav-link" href="#">
+                Contact
+              </a>
+            </li>
+            <button className="btn customButton">Contact us</button>
+          </ul>
         </div>
+      </nav>
+    </div>
   );
 };
 
